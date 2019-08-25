2f55c426dc4e584f342f25e3c339665d026b1c0f
Pablo Andrés Díaz Mazzaro <diazmazzaro@gmail.com>
2019-08-25 20:08:42 -0300

    Check and create user data directory

--- main.py
+++ main.py
@@ -6,9 +6,32 @@
 # |__|  |__|     \__/  \__/          \___/  
 #                                           
 
-# Request user to enter File name
+# Dependencies
+import os
+
+# Request user to enter File name.
 def reqFileName ():
-  name = input("Please enter File name: ")
+  name = input("Please enter \033[1;32;40mFile\033[0;37;40m name: ").lower()
+  if len(name) > 4 and '.txt' in name:
+    return name
+  else:
+    print("\033[0;31;40mInvalid \033[1;31;40mFile name.\033[0;37;40m ")
+    return reqFileName()
 
+# 
+def checkUsrData ():
+  path = ".usrdata"
+  if not os.path.isdir(path):
+    os.mkdir(path)
 
-reqFileName()
+# Checks if file already exists and prompt options
+def chekFile (file):
+  if os.path.isfile(".usrdata/" + path):
+    action = input("Please enter \033[1;32;40mFile\033[0;37;40m name: ").lower()
+  else:
+    name = input("You are creating a \033[1;36;40mnew File\033[0;37;40m.Please enter  name: ").lower()
+
+  return
+
+file_name = reqFileName()
+checkUsrData()
