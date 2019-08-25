fa0f3bb4e81caf402aab9c68dcfd1874d2466044
Pablo Andrés Díaz Mazzaro <diazmazzaro@gmail.com>
2019-08-25 19:43:05 -0300

    Request file name

---
+++ main.py
@@ -0,0 +1,14 @@
+#  __    __  ____    __    ____       ___   
+# |  |  |  | \   \  /  \  /   /      / _ \  
+# |  |__|  |  \   \/    \/   / _____| (_) | 
+# |   __   |   \            / |______> _ <  
+# |  |  |  |    \    /\    /        | (_) | 
+# |__|  |__|     \__/  \__/          \___/  
+#                                           
+
+# Request user to enter File name
+def reqFileName ():
+  name = input("Please enter File name: ")
+
+
+reqFileName()
